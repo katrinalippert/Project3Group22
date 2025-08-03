ecb44a34aea714d121ddd593d5e702fb6b9ebf9e
kal200 <kalippert35@gmail.com>
2025-08-03 16:58:16 -0400

    cleaned main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,49 +9,8 @@ using namespace std;
 
 
 int main() {
-    cout << "TEST" << endl;
-
-    // ParseData test;
-    // cout << works << endl;
-
-    //
-    // auto start = std::chrono::system_clock::now();
-    // test.readCsv("food_data.csv", false);
-    // test.printMaps();
-    // test.printSizes();
-    // auto end = std::chrono::system_clock::now();
-    // auto duration = chrono::duration_cast<chrono::seconds>(end-start);
-
-    // cout << "DURATION: " << duration.count() << endl;
-    //
-    //
-    // test.printSizes();
-
-
-
-    // //
-    // test.setNames({1105904, 1105905}, apiKey);
-    // //
-    // test.printMaps();
-
     UI testUI;
     testUI.StartUI();
-
-    // sf::RenderWindow testWindow(sf::VideoMode(800,800), "TEST");
-    // while (testWindow.isOpen()) {
-    //     sf::Event event;
-    //     while (testWindow.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed) {
-    //             testWindow.close();
-    //         }
-    //     }
-    //
-    //
-    //     testWindow.clear(sf::Color::Blue);
-    //
-    //     testWindow.display();
-    // }
-
 
 
     return 0;
